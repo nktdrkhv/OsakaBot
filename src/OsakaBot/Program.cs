a70e40e84ac302cbe59b493b517729ae4b8d552d
Nikita Dorokhov <nktdrkhv@outlook.com>
2023-08-12 19:15:11 +0700

    Init

---
+++ src/OsakaBot/Program.cs
@@ -0,0 +1,10 @@
+using Osaka.Bot;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
